6f6b30c84745a4d683619e5985265f203e1e79ed
toxa4elovek <toxa4elovek@gmail.com>
2017-11-03 00:58:55 +0300

    Добавление модального окна

--- frontend/modules/mainpage/controllers/DefaultController.php
+++ frontend/modules/mainpage/controllers/DefaultController.php
@@ -34,7 +34,9 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
-        $weather = $this->getData(1, 'meteo', ['date' => date('d-m-Y', time())]);
+        $date = str_replace(' ', '%20', date('d-m-Y H:i:s', time()));
+        $popupWindow = [];
+        $weather = $this->getData(1, 'meteo', ['date' => $date]);
 
         foreach ((array)$weather['WEATHER_DATA'] as &$item){
             AcmoApi::check($item);
@@ -42,9 +44,66 @@ class DefaultController extends Controller
                 'name' => $item['METEO_NAME'],
                 'lat' => $item['latitude'],
                 'lon' => $item['longitude'],
-                'render' => $this->renderPartial('_popup_window', ['item' => $item]),
+                'temperature' => $item['T'],
+                'render' => $this->renderPartial('_popup_window'),
             ];
         }
+        /*$result = [];
+        $transport = [];
+        $forecast = [];
+        foreach ($weather['WEATHER_DATA'] as $item){
+            $imgList = $this->getData(1, 'videolist', [
+                'id'=> $item['METEO_ID'],
+                'date' => $date
+            ]);
+            $tm = $this->getData(1, 'tm', [
+                'id'=> $item['METEO_ID'],
+                'date' => str_replace(' ', '%20', date('d-m-Y H:i:s', time() - 7200 *5))
+            ]);
+            foreach ($tm['TM_DATA'] as $t){
+                BaseAPI::check($t);
+                $transport[] = [
+                    'car' => $t['Car'],
+                    'truck' => $t['STruck'] + $t['MTruck'] + $t['LTruck'] + $t['BTruck'] + 0,
+                    'bus' => $t['Bus']
+                ];
+            }
+            $forecast['now'] = $this->getData(1, 'forecast', ['id' => $item['METEO_ID'], 'date' => $date]);
+            $forecast['now_4'] = $this->getData(1, 'forecast', [
+                'id' => $item['METEO_ID'],
+                'date' => str_replace(' ', '%20', date('d-m-Y H:i:s', time() + 3600 * 4))
+            ]);
+            $forecast['now_8'] = $this->getData(1, 'forecast', [
+                'id' => $item['METEO_ID'],
+                'date' => str_replace(' ', '%20', date('d-m-Y H:i:s', time() + 3600 * 8))
+            ]);
+            $forecast['now_12'] = $this->getData(1, 'forecast', [
+                'id' => $item['METEO_ID'],
+                'date' => str_replace(' ', '%20', date('d-m-Y H:i:s', time() + 3600 * 12))
+            ]);
+
+            foreach ($forecast as &$value){
+                BaseAPI::check($value['WEATHER_DATA']);
+            }
+
+            $result = [
+                'name' => $item['METEO_NAME'],
+                'img' => $this->getModelApi(1)->getVideoByVideoList($imgList['VIDEO_DATA']),
+                't' => $item['T'],
+                'date' => date('d.m.Y H:i', strtotime($item['WEATHER_DATE'])),
+                't_road' => $item['t_road'],
+                'u' => $item['U'],
+                'po' => $item['PO'],
+                'road_state' => (!empty($item['road_state'])) ? AcmoApi::$road_state[$item['road_state']] : '',
+                'adhesion' => $item['ADHESION'],
+                'prec_type' => (!empty($item['PREC_TYPE'])) ? AcmoApi::$prec_type[$item['PREC_TYPE']] : '',
+                'prec_sum' => $item['PREC_SUM'],
+                'ff' => $item['FF'],
+                'transport' => $transport,
+                'forecast' => $forecast
+            ];
+            break;
+        }*/
 
         return $this->render('index', [
             'popupWindow' => $popupWindow
@@ -52,16 +111,25 @@ class DefaultController extends Controller
     }
 
     /**
-     * @param $id
+     * @param $region_id
      * @param $type
      * @param $data
      * @return mixed|null
      */
-    protected function getData($id, $type, $data)
+    protected function getData($region_id, $type, $data)
     {
-        if($region = Region::findOne($id)){
+        if($region = Region::findOne($region_id)){
             return AcmoApi::get($region->url)->getData($type, $data);
         }
+        return null;
+    }
+
+    protected function getModelApi($region_id)
+    {
+        if($region = Region::findOne($region_id)){
+            return AcmoApi::get($region->url);
+        }
+
         return null;
     }
 }
--- frontend/modules/mainpage/views/default/_popup_window.php
+++ frontend/modules/mainpage/views/default/_popup_window.php
@@ -4,8 +4,164 @@
  */
 ?>
 
-<?php foreach ($item as $value):?>
-    <ul>
-        <li><?php echo $value?></li>
-    </ul>
-<?php endforeach;?>
+
+<!-- start map-modal.html-->
+<div class="map__modal">
+    <div class="map__modal--header">
+        <span>А151 км 35+700 Шихазаны</span>
+    </div>
+    <div class="map__modal--blocks">
+        <div class="map__modal--block">
+            <div class="map__modal--video">
+                <div class="video">
+                    <img id="img1" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img2" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img3" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img4" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img5" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img6" class="tabcontent" src="img/place.png" alt="">
+                    <img id="img7" class="tabcontent" src="img/place.png" alt="">
+                    <div>
+                        <span></span>
+                        <b>12.10.2016 12:00</b>
+                        <ul>
+                            <li class="tablinks"><a href="#img1"></a></li>
+                            <li class="tablinks"><a href="#img2"></a></li>
+                            <li class="tablinks"><a href="#img3"></a></li>
+                            <li class="tablinks"><a href="#img4"></a></li>
+                            <li class="tablinks"><a href="#img5"></a></li>
+                            <li class="tablinks"><a href="#img6"></a></li>
+                            <li class="tablinks"><a href="#img7"></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="cars-type">
+                    <div>
+                        <p>Легковые</p>
+                        <span>143</span>
+                        <span>92</span>
+                    </div>
+                    <div>
+                        <p>Грузовые</p>
+                        <span>112</span>
+                        <span>89</span>
+                    </div>
+                    <div>
+                        <p>Автобусы</p>
+                        <span>12</span>
+                        <span>9</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="map__modal--block">
+            <div class="map__modal--center-top">
+                <img class="map-star" src="img/icons/star.png" alt="">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Т воздух</th>
+                        <th><img src="img/icons/table-weather.png" alt=""></th>
+                        <th><span>-3</span> °C</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Влажность</td>
+                        <td></td>
+                        <td>95%</td>
+                    </tr>
+                    <tr>
+                        <td>Давление</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Осадки</td>
+                        <td>Снег</td>
+                        <td>0,5 мм</td>
+                    </tr>
+                    <tr>
+                        <td>Ветер</td>
+                        <td><img src="img/icons/wind-direction-dark.png" alt=""></td>
+                        <td>15 м/с</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="map__modal--center-bottom">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Т поверх.:</td>
+                        <td class="color-blue">-5 °C</td>
+                    </tr>
+                    <tr>
+                        <td>Состояние</td>
+                        <td class="color-white">Сухо</td>
+                    </tr>
+                    <tr>
+                        <td>Сцепление</td>
+                        <td class="color-green">0,8</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="map__modal--block">
+            <div class="map__modal--forecast">
+                <table>
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Погода</th>
+                        <th>Ветер</th>
+                        <th>Воздух</th>
+                        <th>Дорога</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="bg-green">Сейчас</td>
+                        <td><img src="img/icons/yellow-star.png" alt=""></td>
+                        <td><img src="img/icons/wind.png" alt=""></td>
+                        <td>-2</td>
+                        <td>-1</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-yellow">00:30</td>
+                        <td><img src="img/icons/cloud.png" alt=""></td>
+                        <td><img src="img/icons/wind.png" alt=""></td>
+                        <td>-1</td>
+                        <td>-2</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-grey">04:30</td>
+                        <td><img src="img/icons/yellow-star.png" alt=""></td>
+                        <td><img src="img/icons/wind.png" alt=""></td>
+                        <td>-2.0</td>
+                        <td>-1,3</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-grey">08:30</td>
+                        <td><img src="img/icons/cloud.png" alt=""></td>
+                        <td><img src="img/icons/wind.png" alt=""></td>
+                        <td>-2</td>
+                        <td>-1,1</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <ul class="map__modal--review">
+                    <li><span class="bg-red"></span>Остановки в движении</li>
+                    <li><span class="bg-yellow"></span>Медленное движение</li>
+                    <li><span class="bg-green"></span>Бесперебойное движение</li>
+                    <li><span class="bg-grey"></span>Нет данных</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end map-modal.html-->
